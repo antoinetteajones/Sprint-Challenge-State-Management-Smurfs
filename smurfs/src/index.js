4e2e18d9f49f266cfab2bbb6eb141efa0c6639c8
Antoinette <create@aplusmedia.info>
2020-02-07 23:52:26 -0500

    done

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
+import {Provider} from "react-redux";
+import logger from "redux-logger";
+import {reducer} from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
